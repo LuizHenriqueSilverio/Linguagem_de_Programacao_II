4268a7d082822dc9244c1e1a7f38695169cac074
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-02-15 08:27:46 -0300

    Aula 15_02_2023

--- AulasLPII/src/banco/Conta.java
+++ AulasLPII/src/banco/Conta.java
@@ -11,11 +11,29 @@ package banco;
  */
 public class Conta {
     String cliente;
-    double saldo;
+    double saldo = 0;
     double limite;
     
     public void deposita(double deposito) {
         this.saldo += deposito;
         System.out.println("Deposito efetuado com sucesso.");
     }
+    
+    public boolean saca(double saque) {
+        if(this.saldo < saque) {
+            return false;
+        }
+        this.saldo -= saque;
+        return true;
+    }
+    
+    public boolean transferir(Conta conta, double valor) {
+        if(this.saldo < valor) {
+            return false;
+        }
+        this.saldo -= valor;
+        conta.saldo += valor;
+        return true;
+    }
+    
 }
