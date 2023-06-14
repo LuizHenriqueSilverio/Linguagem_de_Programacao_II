7cf5c1a9d18603ae1e88b3edaf329fe833f7e1ef
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-06-14 08:05:00 -0300

    Atualizado - aula 14/06/2023

--- EstudoCaso/backup_BDEstudoCaso.sql
+++ EstudoCaso/backup_BDEstudoCaso.sql
@@ -25,10 +25,14 @@ CREATE TABLE IF NOT EXISTS `cidade` (
   `nome` varchar(200) NOT NULL DEFAULT '',
   `UF` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`codCidade`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela estudocaso3si.cidade: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela estudocaso3si.cidade: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
+INSERT INTO `cidade` (`codCidade`, `nome`, `UF`) VALUES
+	(1, 'Machado', 'MG'),
+	(2, 'Paraguaçu', 'MG'),
+	(4, 'São Paulo', 'SP');
 /*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estudocaso3si.funcionario
@@ -42,10 +46,12 @@ CREATE TABLE IF NOT EXISTS `funcionario` (
   PRIMARY KEY (`codFuncionario`),
   KEY `FK_funcionario_cidade` (`cidade_codCidade`),
   CONSTRAINT `FK_funcionario_cidade` FOREIGN KEY (`cidade_codCidade`) REFERENCES `cidade` (`codCidade`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- Copiando dados para a tabela estudocaso3si.funcionario: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela estudocaso3si.funcionario: ~1 rows (aproximadamente)
 /*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` (`codFuncionario`, `nome`, `salario`, `nascimento`, `cidade_codCidade`) VALUES
+	(1, 'Luiz Henrique', 3500, '1999-11-25', 4);
 /*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
